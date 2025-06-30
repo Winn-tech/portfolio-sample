0e80e366fe9e7e551a180d088bf84ef2a81562b1
winn-tech <olugodwin99@gmail.com>
2025-06-30 10:25:35 +0100

    a little update

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const menuIcon = document.getElementById('menu')
+const sidebarCenter = document.getElementById('sidebarCenter')
 const closeButton = document.getElementById('close')
 const sideBar = document.getElementById('sidebar')
 
 menuIcon.addEventListener('click', function(){
     sideBar.style.display = 'block'
+    sideBar.classList.add('show')
 })
 
 closeButton.addEventListener('click', function(){
